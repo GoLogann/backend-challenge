7494a550f733958e7f318cef8dbdaa7f2e7dcf39
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-02-14 18:53:08 -0300

    feat: created connection for mongoDB

---
+++ internal/repository/mongo.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoDB struct {
+	Client   *mongo.Client
+	Database *mongo.Database
+}
+
+func NewMongoDB(uri, dbName string) (*MongoDB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+
+	log.Println("Connected to MongoDB!")
+
+	return &MongoDB{
+		Client:   client,
+		Database: client.Database(dbName),
+	}, nil
+}
+
+func (db *MongoDB) Close() {
+	if err := db.Client.Disconnect(context.Background()); err != nil {
+		log.Fatalf("Error closing MongoDB connection: %v", err)
+	}
+	log.Println("MongoDB connection closed.")
+}
